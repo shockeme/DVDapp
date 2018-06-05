3691423b98c04961609dcdcd59d7a545d29d80f1
Steve Hockemeier <shockeme@yahoo.com>
2018-06-04 18:06:41 -0700

    Add project files.

---
+++ DVDapp2/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DVDapp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 94);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 63);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(267, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 97);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Pause";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(266, 137);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 90);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Fullscreen";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(265, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 86);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Mute";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(481, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(481, 76);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(134, 76);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "timestamp";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(480, 158);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(135, 86);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Skip";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(478, 269);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(145, 70);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "nextChapter";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(638, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(650, 77);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(105, 57);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Select Filter File";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 96);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "5 sec rewind";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(147, 343);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(521, 32);
+            this.hScrollBar1.TabIndex = 13;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(483, 9);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(657, 163);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(97, 63);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Chap Info";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(658, 265);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(96, 73);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "FastPlay";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(148, 229);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(82, 97);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "5 sec ffwd";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(654, 141);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "Force Filter Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 378);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Int64 MyDvd;
+        private Int64 MyMediaPlayer;
+        private Int64 TotalMovieLength;
+        private StreamReader myFilterFile;
+        private string[] FilterFileEntry;
+        private List<string[]> FilterFileEntries;
+        private Int64 NextTimeEvent;
+        private int CurrentFilterIndx;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ DVDapp2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace DVDapp2
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern Int64 libvlc_new(int argc, string argv);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern Int64 libvlc_media_new_location(Int64 p_instance, string psz_mrl);
+
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern Int64 libvlc_media_player_new_from_media(Int64 p_md);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_media_player_play(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_media_player_stop(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_media_player_pause(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_toggle_fullscreen(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_set_fullscreen(Int64 p_mi, int EnableFullscreen);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_audio_toggle_mute(Int64 p_mi);
+
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern Int64 libvlc_media_player_get_time(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_media_player_set_time(Int64 p_mi, Int64 i_time);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_media_player_next_chapter(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern Int64 libvlc_media_player_get_length(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_media_player_get_chapter(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_media_player_get_title(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern void libvlc_media_player_set_title(Int64 p_mi, int i_title);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_video_get_track(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_video_set_track(Int64 p_mi, int i_track);
+
+        //[DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        //LIBVLC_API int libvlc_media_player_get_full_title_descriptions(libvlc_media_player_t* p_mi, libvlc_title_description_t*** titles);
+        //[DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        //LIBVLC_API int libvlc_media_player_get_full_chapter_descriptions(libvlc_media_player_t* p_mi, int i_chapters_of_title, libvlc_chapter_description_t*** pp_chapters);
+        //[DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        //LIBVLC_API libvlc_track_description_t * 	libvlc_video_get_track_description(libvlc_media_player_t* p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_video_get_track_count(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_media_player_get_chapter_count(Int64 p_mi);
+        [DllImport("C:\\Program Files\\VideoLAN\\VLC\\libvlc.dll")]
+        public static extern int libvlc_media_player_get_title_count(Int64 p_mi);
+
+        public Form1()
+        {
+            InitializeComponent();
+            MyDvd = libvlc_new(0, "");
+            MyMediaPlayer = libvlc_media_player_new_from_media(libvlc_media_new_location(MyDvd, "dvd:///d:/"));
+            FilterFileEntries = new List<string[]>();
+            CurrentFilterIndx = 0;
+            // init filter to something bogus until loaded
+            FilterFileEntry = new string [] { "9999999999", "", "9999999999" };
+            FilterFileEntries.Add(FilterFileEntry);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // start filter events at beginning, or should check where current postion, as this button can also unpause
+            CurrentFilterIndx = 0;
+            FilterFileEntry = FilterFileEntries[CurrentFilterIndx];
+            NextTimeEvent = Convert.ToInt64(FilterFileEntry[0]);
+
+            libvlc_media_player_play(MyMediaPlayer);
+            timer1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            libvlc_media_player_stop(MyMediaPlayer);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            libvlc_media_player_pause(MyMediaPlayer);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            libvlc_toggle_fullscreen(MyMediaPlayer);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            libvlc_audio_toggle_mute(MyMediaPlayer);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = libvlc_media_player_get_time(MyMediaPlayer).ToString();
+            textBox3.Text = libvlc_media_player_get_length(MyMediaPlayer).ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            libvlc_media_player_set_time(MyMediaPlayer, Convert.ToInt64(textBox1.Text));
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            libvlc_media_player_next_chapter(MyMediaPlayer);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //StreamReader myfilestream;
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.SafeFileName;
+                myFilterFile = new StreamReader(openFileDialog1.FileName);
+                FilterFileEntries.Clear();
+                do
+                {
+                    FilterFileEntry = myFilterFile.ReadLine().Split(',');
+                    FilterFileEntries.Add(FilterFileEntry);
+                    textBox1.Text = FilterFileEntry[0];
+                } while (!myFilterFile.EndOfStream);
+                myFilterFile.Close();
+            }
+            // reload first time event, really, should do this relative to current playing position
+            //while(Entry.. < currenttime)
+            //{
+            CurrentFilterIndx = 0;
+            FilterFileEntry = FilterFileEntries[CurrentFilterIndx];
+            NextTimeEvent = Convert.ToInt64(FilterFileEntry[0]);
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // title 1 appears to be main event (ie. the movie); only run this code once the main event starts
+            // seems this is not always the case, which breaks fastplay, too; so, allow option to force
+            // until figure out how to detect main event
+            if((libvlc_media_player_get_title(MyMediaPlayer) == 1) || (checkBox1.Checked))
+            {
+                // check if we've hit next time event
+                if (libvlc_media_player_get_time(MyMediaPlayer) >= NextTimeEvent)
+                {
+                    if (FilterFileEntry[1] == "mute")
+                    {
+                        libvlc_audio_toggle_mute(MyMediaPlayer);
+                        // setup unmute event (still uses 'mute' keyword); only do this once
+                        if (Convert.ToInt64(FilterFileEntry[2]) > NextTimeEvent)
+                        {
+                            NextTimeEvent = Convert.ToInt64(FilterFileEntry[2]);
+                        }
+                        else
+                        {
+                            CurrentFilterIndx++;
+                            if (CurrentFilterIndx < FilterFileEntries.Count)
+                            {
+                                FilterFileEntry = FilterFileEntries[CurrentFilterIndx];
+                                NextTimeEvent = Convert.ToInt64(FilterFileEntry[0]);
+                            }
+                            else
+                            {
+                                NextTimeEvent = 9999999999; // set to value that is too large
+                                // disable this timer, since we don't need it anymore
+                                //timer1.Enabled = false;
+                            }
+                        }
+
+                    }
+                    else if (FilterFileEntry[1] == "skip")
+                    {
+                        libvlc_media_player_set_time(MyMediaPlayer, Convert.ToInt64(FilterFileEntry[2]));
+                        CurrentFilterIndx++;
+                        if (CurrentFilterIndx < FilterFileEntries.Count)
+                        {
+                            FilterFileEntry = FilterFileEntries[CurrentFilterIndx];
+                            NextTimeEvent = Convert.ToInt64(FilterFileEntry[0]);
+                        }
+                        else
+                        {
+                            NextTimeEvent = 9999999999; // set to value that is too large
+                                                          // disable this timer, since we don't need it anymore
+                            //timer1.Enabled = false;
+                        }
+                    }
+                }
+            }
+            //update progress on trackbar
+            hScrollBar1.Maximum = (int)libvlc_media_player_get_length(MyMediaPlayer);
+            // sometimes it seems out of context and hscrollbar values are not valid???
+            if (hScrollBar1.Maximum != 0)
+            {
+                hScrollBar1.Value = (int)libvlc_media_player_get_time(MyMediaPlayer);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            libvlc_media_player_set_time(MyMediaPlayer, libvlc_media_player_get_time(MyMediaPlayer) - 5000);
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            libvlc_media_player_set_time(MyMediaPlayer, hScrollBar1.Value);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = libvlc_media_player_get_chapter(MyMediaPlayer).ToString();
+            textBox2.Text = libvlc_media_player_get_title(MyMediaPlayer).ToString();
+            textBox3.Text = libvlc_video_get_track(MyMediaPlayer).ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            // seems that title is generally the main event...
+            libvlc_media_player_set_title(MyMediaPlayer, 1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            libvlc_media_player_set_time(MyMediaPlayer, libvlc_media_player_get_time(MyMediaPlayer) + 5000);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+    }
+}
